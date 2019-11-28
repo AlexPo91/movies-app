4828c8cf4a96af8e3e4db4bd9180bfc2ce5debe1
Pasha <matrunarto@desoz.com>
2019-11-28 19:32:17 +0300

    Add some notes

--- src/components/MoviesApp/Header/Header.jsx
+++ src/components/MoviesApp/Header/Header.jsx
@@ -1,38 +1,50 @@
 import React from 'react'
 import styles from './styles.module.css'
 import ButtonSearch from './ButtonSearch'
-export default function Header(props) {
-console.log(props)
-console.log(props.option)
+
+
+function RadionButton({ checked, onChange }) {
+    return (
+        <input
+            name="lang"
+            type="radio"          
+            checked={checked}
+            onChange={onChange}
+    />
+    )
+}
+
+
+
+
+export default function Header({ onChange, onRadioChange, value, option, onClick }) {
+    console.log(option)
     return (
         <div className= {styles.header}>
         <h1 className ={styles.title}>
             Search Films
         </h1>
-        <form onSubmit = {props.onChange}>
+        <form onSubmit = {onChange}>
             <span className = {styles.subtitle}>
                 find your movie
             </span>
             <div className = {styles.searchForm}>
             <div>
-            <input
-				name="lang"
-				type="radio"
-                value="title"
-				onChange={props.onRadioChange}
+            <RadionButton
+                checked={option === 'title'}
+				onChange={() => onRadioChange('title')}
 			/>
-
-			<input
-				name="lang"
-				type="radio"
-                value="genres"
-				onChange={props.onRadioChange}
+             <RadionButton
+                hecked={option === 'genres'}
+				onChange={() => onRadioChange('genres')}
+               
 			/>
+			
       </div>
-                <input type="text" value={props.value} onChange = {props.onChange}/>
+                <input type="text" value={value} onChange = {onChange}/>
                 <div className = {styles.buttonPanel}>
                     <span className = {styles.text}>Search By</span>
-                    <ButtonSearch  onClick = {props.onClick} option = {props.option}/>
+                    <ButtonSearch  onClick = {onClick} option = {option}/>
                 </div>
             </div>
         </form>
--- src/components/MoviesApp/MoviesApp.jsx
+++ src/components/MoviesApp/MoviesApp.jsx
@@ -2,41 +2,74 @@ import React, {useState} from 'react'
 import Header from './Header'
 import ShowMovies from './ShowMovies'
 import SortBy from './SortBy'
+
+
+
+function Modal({ visible, onClick, children }) {
+    return <div onClick={onClick} style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, backgroundColor: 'rgba(0,0,0,0.4)' }}>
+        {children}
+    </div>
+}
+
+
+function fetchMovies(value, option) {
+    return fetch(`https://reactjs-cdp.herokuapp.com/movies?sortOrder=desc&search=${value}&searchBy=${option}&limit=20`)
+    .then(data => { return data.json()})
+}
+
+const SEARCH_OPTIONS = {
+    TITLE: 'title',
+    GANRE: 'ganre'
+}
+
+function sortByRating(data) {
+    return [...data].sort((a,b) => b.vote_average - a.vote_average) 
+}
+
+
 export default function MoviesApp(props) {
     
     const [value, setValue] = useState('')
-    const [url, setUrl] = useState('')
-    const [option, setOption] = useState('title')
-    const handleRadioChange =event => {
-        setOption(event.nativeEvent.target.value)
+    const [option, setOption] = useState(SEARCH_OPTIONS.TITLE)
+    const [movie, setMovie] = useState([])
+
+    const handleRadioChange =nextOption => {
+        setOption(nextOption)
     }
-    console.log(option)
-    console.log(value)
+
     const handleChange = event => {
         event.preventDefault()
         setValue(event.target.value)
-          setUrl (`https://reactjs-cdp.herokuapp.com/movies?sortOrder=desc&search=${value}&searchBy=${option}&limit=20`)
-        }
-    const [movie, setMovie] = useState([])
-    const moviesData = (event) => {
-        event.preventDefault()
-            fetch(url)
-        .then(data => {return data.json()})
-        .then(parseData => {setMovie(()=>parseData.data.map(item => item))})
-        }
-    const btnSortRating = () => {
-        setMovie(() => (movie.sort((a, b) => b.vote_average - a.vote_average)).map(item => item) )
-        console.log(movie)
     }
+
+
+    const moviesData = async (event) => {
+        event.preventDefault()
+        const { data } = await fetchMovies(value, option)    
+        setMovie(data)
+    }
+
+    const btnSortRating = () => {
+        const sortedByRating = sortByRating(movie)
+        setMovie(sortedByRating)      
+    }
+
     const btnSortDate = () => {
-        setMovie(() => movie.sort((a, b) => b.release_date.split('-')[0] - a.release_date.split('-')[0]).map(item => item))
-        console.log(movie)
+        const sortedByData = [...movie].sort((a,b) => {
+            return new Date(b.release_date).getFullYear() - new Date(a.release_date).getFullYear()
+        } )
+
+        setMovie(sortedByData)
     }
     return (
         <React.Fragment>
-            <Header value={value} onRadioChange = {handleRadioChange} onChange = {handleChange} url = {url} onClick = {moviesData}/>
+            <Header value={value} onRadioChange = {handleRadioChange} onChange = {handleChange} onClick = {moviesData} option={option}/>
             <SortBy btnSortRating = {btnSortRating} btnSortDate = {btnSortDate} movie = {movie}/>
             <ShowMovies movie = {movie} />
+            {/* <Modal visible={true} onBackgroundClock >
+                <MovieDetails id />
+            </Modal> */}
+            {/* <Modal visible={true} /> */}
         </React.Fragment>
     )
 }
